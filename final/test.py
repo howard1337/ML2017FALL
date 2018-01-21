d7bc6bef05ff065ade1c74fb60e5f725bc935e6a
howard1337 <y60091534@gmail.com>
2018-01-21 18:31:09 +0800

    fix subprocess bug

--- final/test.py
+++ final/test.py
@@ -11,5 +11,5 @@ with open('used','r') as fp:
 		line = line.strip('\r\n').replace('\\','')
 		line = os.path.join('models',line)
 		print("python3 chat_upload.py " + line + " " + str(index) + ".csv " + test_path)
-		subprocess.call("python3 chat_upload.py " + line + " " + str(index) + ".csv " + test_path)
-	subprocess.call("python3 model_merge.py " + output_path + " 1.csv 2.csv 2.csv 3.csv 4.csv 4.csv 5.csv 6.csv 7.csv 8.csv 8.csv 9.csv " )
+		subprocess.call("python3 chat_upload.py " + line + " " + str(index) + ".csv " + test_path, shell = True)
+	subprocess.call("python3 model_merge.py " + output_path + " 1.csv 2.csv 2.csv 3.csv 4.csv 4.csv 5.csv 6.csv 7.csv 8.csv 8.csv 9.csv ", shell = True)
